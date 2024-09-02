0b6caf0e6e9f2cc4a80380686dcf39619a2bad18
Mofizul Islam <92608521+Mofizul-Islam@users.noreply.github.com>
2024-09-02 19:48:14 +0530

    some changes

--- db/mysql.py
+++ db/mysql.py
@@ -1,53 +1,55 @@
-import MySQLdb._exceptions
-import MySQLdb._mysql
-from myutils import DB_HOST, DB_USER, DB_PASSWORD, DB_PORT, DB_NAME
-from flask_mysqldb import MySQL
-from flask import Flask
-import MySQLdb.cursors
+# import MySQLdb._exceptions
+# import MySQLdb._mysql
+# from myutils import DB_HOST, DB_USER, DB_PASSWORD, DB_PORT, DB_NAME
+# from flask_mysqldb import MySQL
+# from flask import Flask
+# import MySQLdb.cursors
 
-mysql = None
-
-
-def init(app: Flask):
-    global mysql
-    app.config['MYSQL_HOST'] = DB_HOST
-    app.config['MYSQL_USER'] = DB_USER
-    app.config['MYSQL_PORT'] = 3306
-    app.config['MYSQL_PASSWORD'] = DB_PASSWORD
-    app.config['MYSQL_DB'] = DB_NAME
-    app.config["MYSQL_CURSORCLASS"] = "DictCursor"
-
-    print("DB", DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
-
-    mysql = MySQL(app)
-    mysql.init_app(app)
-
-    print("MYSQL", mysql.connection, MySQLdb.apilevel)
-
-    return mysql
+# mysql = None
 
 
-def execute_query(query: str, params: tuple):
-    if not mysql:
-        raise Exception("MySQL Connection not initialized")
+# def init(app: Flask):
+#     global mysql
+#     app.config['MYSQL_HOST'] = DB_HOST
+#     app.config['MYSQL_USER'] = DB_USER
+#     app.config['MYSQL_PORT'] = 3306
+#     app.config['MYSQL_PASSWORD'] = DB_PASSWORD
+#     app.config['MYSQL_DB'] = DB_NAME
+#     app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 
-    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+#     print("DB", DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
 
-    cursor.execute(query, params)
+#     mysql = MySQL(app)
+#     mysql.init_app(app)
 
-    return cursor
+#     print("MYSQL", mysql.connection, MySQLdb.apilevel)
 
-
-def fetch_all(query: str, params: tuple):
-    cursor = execute_query(query, params)
-    return cursor.fetchall()
-
-
-def fetch_one(query: str, params: tuple):
-    cursor = execute_query(query, params)
-    return cursor.fetchone()
+#     return mysql
 
 
-def execute(query: str, params: tuple):
-    cursor = execute_query(query, params)
-    return cursor.lastrowid
+# def execute_query(query: str, params: tuple):
+#     if not mysql:
+#         raise Exception("MySQL Connection not initialized")
+
+#     cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+
+#     cursor.execute(query, params)
+
+#     return cursor
+
+
+# def fetch_all(query: str, params: tuple):
+#     cursor = execute_query(query, params)
+#     return cursor.fetchall()
+
+
+# def fetch_one(query: str, params: tuple):
+#     cursor = execute_query(query, params)
+#     return cursor.fetchone()
+
+
+# def execute(query: str, params: tuple):
+#     cursor = execute_query(query, params)
+#     return cursor.lastrowid
+
+print("mysql.py")
